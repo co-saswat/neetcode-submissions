b263e1327ce1ca95ff529453ffd1fad22e16efd3
Saswat Sachin Mishra <ssm13092013@gmail.com>
2026-05-02 21:43:31 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    int row, col;
+    public int maxAreaOfIsland(int[][] grid) {
+        row = grid.length;
+        col = grid[0].length;
+        int maxArea = 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0 ; j < col; j++){
+                if(grid[i][j]==1){
+                   int area = bfs(grid,i,j);
+                    maxArea = Math.max(maxArea , area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int bfs(int[][] grid, int i , int j){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i,j});
+        grid[i][j] = 0;
+        int area = 1;
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr >= 0 && nc >= 0 && nr < row && nc < col && grid[nr][nc]==1){
+                    q.offer(new int[]{nr,nc});
+                    grid[nr][nc] = 0;
+                    area++;
+                }
+            }
+        }
+
+        return area;
+    }
+}
